3016d578ade480b47a8e277436450d729ac66573
lovetruongan <lovetruongan@gmail.com>
2024-05-31 09:40:37 +0700

    Add Terraform file

---
+++ main.tf
@@ -0,0 +1,38 @@
+resource "google_service_account" "default" {
+  account_id   = "terraform-user"
+  display_name = "Shareli"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "Shareli"
+  machine_type = "e2-micro"
+  zone         = "asia-northeast1-b"
+
+  tags = ["http-server", "https-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "apt-get update -y && sudo apt-get install -y docker.io"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+    project = "shareli-gemini-2024"
+    region = "asia-northeast1-b"
+    credentials = "shareli-gemini-2024-caa1eb06497b.json"
+}
